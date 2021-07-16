c40deff28884f710db618caf2d7571fd62d4f3fc
SergeiHerashenko <46726784+SergeiHerashenko@users.noreply.github.com>
2021-07-16 17:52:23 +0300

    Task_Four

---
+++ Task_Four/Task_Four/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace Task_Four
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Перший варіант(Дуже великий розмір масиву)
+            string[,,,] Max = new string[34000, 100023, 30003, 220001];
+
+            //Другий варіант(Максимальна місткість один символ, а вставляємо два символи)
+            StringBuilder sb = new StringBuilder(1, 1);
+            sb.Insert(0, "x", 2);
+        }
+    }
+}
